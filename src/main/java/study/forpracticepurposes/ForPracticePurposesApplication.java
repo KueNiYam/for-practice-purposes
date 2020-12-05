efe5029bd7bca6da0ddb4e2bf1a7d4a09c3aa505
Kuenhwi <34573901+KueNiYam@users.noreply.github.com>
2020-12-06 02:26:43 +0900

    init: for-practice-purposes

---
+++ src/main/java/study/forpracticepurposes/ForPracticePurposesApplication.java
@@ -0,0 +1,13 @@
+package study.forpracticepurposes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForPracticePurposesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForPracticePurposesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/forpracticepurposes/ForPracticePurposesApplicationTests.java
@@ -0,0 +1,13 @@
+package study.forpracticepurposes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForPracticePurposesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
